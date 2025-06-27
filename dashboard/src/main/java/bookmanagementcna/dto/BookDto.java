d89502694550c9116c8e8f933577b2debd345236
tk032 tk032 <tedyang032@gmail.com>
2025-06-27 07:38:06 +0000

    Dto, Controller 작성

---
+++ dashboard/src/main/java/bookmanagementcna/dto/BookDto.java
@@ -0,0 +1,20 @@
+package bookmanagementcna.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String contentSummary;
+    private String coverUrl;
+    private Boolean isAvailable;
+    private Date publishedDate;
+}
